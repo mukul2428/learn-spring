e4525b60c2649b3d2189ca6d245f71037bcb0b9d
mukul2428 <mukulraghav28@gmail.com>
2024-11-25 18:36:37 +0530

    fix: added comments

--- src/main/java/com/viralcoder/learnspring/App03GamingSpringBeans.java
+++ src/main/java/com/viralcoder/learnspring/App03GamingSpringBeans.java
@@ -3,8 +3,10 @@ import com.viralcoder.learnspring.game.GameRunner;
 import com.viralcoder.learnspring.game.GamingConsole;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+// this is using spring- loosely coupled
 public class App03GamingSpringBeans {
     public static void main(String[] args) {
+        // launching spring context, GamingConfiguration will have all beans
         try(var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
             // printed supercontra/mario up method using gamingconsole interface
             context.getBean(GamingConsole.class).up();
--- src/main/java/com/viralcoder/learnspring/game/GamingConsole.java
+++ src/main/java/com/viralcoder/learnspring/game/GamingConsole.java
@@ -1,5 +1,6 @@
 package com.viralcoder.learnspring.game;
 
+//this interface is used for making loosely coupled, its methods will be implemented by others
 public interface GamingConsole {
     void up();
     void down();
